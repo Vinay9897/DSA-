446d26588089f893de2d2472c8e7c98fc18ccc09
Vinayyad9897 <vinayyadav91190@gmail.com>
2024-05-07 22:09:35 +0530

    write square pattern

---
+++ src/patterns/SquareStar.java
@@ -0,0 +1,24 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class SquareStar {
+    public static void main(String[] args) {
+        startMethod();
+    }
+
+    public static void startMethod(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the row size of the square: ");
+        int row = scanner.nextInt();
+        System.out.print("Enter the column size of the square: ");
+        int col = scanner.nextInt();
+        for(int i = 0 ; i < row ; i++){
+            for(int j = 0 ; j < col ; j++){
+                System.out.print(" * ");
+            }
+             System.out.println("");
+        }
+        scanner.close();
+    }
+}
